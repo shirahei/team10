91a1995323642befa8da2355d36be745b6113760
shirahei <shirahei@post.bgu.ac.il>
2022-05-28 16:18:27 +0300

    28/05/2022

--- static_p/ActiveNavBar.js
+++ static_p/ActiveNavBar.js
@@ -1,3 +1,5 @@
+window.addEventListener('load', main);
+
 //print date to log
 const d = Date();
 console.log(d);
@@ -16,4 +18,49 @@ const links = document.querySelectorAll('nav a').forEach(link => {
     link.classList.add('active');
   }
 });
+function main() {
 
+  function openForm() {
+    document.getElementById("myForm").style.display = "block";
+  }
+
+  function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+  }
+}
+
+var check = function() {
+  if (document.getElementById('password').value ==
+    document.getElementById('confirm_password').value) {
+    document.getElementById('message').style.fontSize = 'large';
+    document.getElementById('message').style.color = 'green';
+    document.getElementById('message').innerHTML = 'Passwords are matching';
+  } else {
+    document.getElementById('message').style.fontSize = 'large';
+    document.getElementById('message').style.color = 'red';
+    document.getElementById('message').innerHTML = 'Passwords are not matching';
+  }
+
+}
+
+var validateMyForm = function(){
+  if (document.getElementById('password').value ==
+    document.getElementById('confirm_password').value ) {
+    return true;
+  } else {
+    alert("Passwords are not matching");
+    return false;
+  }
+}
+
+var checkNumbers = function(){
+  var ccnumber=document.getElementById('ccnumber').value
+  var cvv=document.getElementById('cvv').value
+  if (ccnumber.length==16 && cvv.length==3 ) {
+    return true;
+  } else {
+    alert("Credit card details are incorrect");
+    return false;
+  }
+
+}
